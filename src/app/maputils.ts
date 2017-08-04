2295e13c2ce96b903f26a7b46826301ec3fb3588
Sergey Zvyagin <bingofirst@gmail.com>
2017-08-04 15:08:19 +0300

    VectorMap workaround for GitHub Pages HTTP response without status text (#20)

--- src/app/maputils.ts
+++ src/app/maputils.ts
@@ -1,5 +1,42 @@
 export class MapUtils {
     static parseMapData(layerDataPath: string, callback: (data: any) => void) {
-        (<any>window).DevExpress.viz.vectormaputils.parse(layerDataPath, { precision: 2 }, callback);
+        Promise.all(this.requestBinarySources(layerDataPath)).then(response => {
+            let layerData = {};
+            response.forEach((value: any) => {
+                layerData[value.key] = value.response;
+            });
+            (<any>window).DevExpress.viz.vectormaputils.parse(layerData, { precision: 2 }, callback);
+        })
+    }
+
+    private static requestBinarySources(sourcePath: string) {
+        return ["shp", "dbf"].map(function(key) {
+            return new Promise((resolve, reject) => {
+                MapUtils.sendRequest(sourcePath + "." + key).then(response => {
+                    resolve({ key: key, response: response });
+                });
+            });
+        });
+    }
+
+    private static sendRequest(url: string) : Promise<any> {
+        return new Promise(
+            (resolve, reject) => {
+                var request = new XMLHttpRequest();
+                request["onreadystatechange"] = function() {
+                    if(this.readyState === 4) {
+                        if(this.status === 200) {
+                            resolve(this.response);
+                        } else {
+                            resolve(null);
+                        }
+                    }
+                };
+                request.open('GET', url);
+                request.responseType = 'arraybuffer';
+                request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+                request.send(null);
+            }
+        )
     }
 }
