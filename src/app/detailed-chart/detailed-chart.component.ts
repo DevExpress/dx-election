da0a7f986a2cd2103f8431c4c86d4e691084fab6
Alexander Tkachenko <24324790+tar-z@users.noreply.github.com>
2019-10-24 13:57:35 +0300

    Use short paths in imports

--- src/app/detailed-chart/detailed-chart.component.ts
+++ src/app/detailed-chart/detailed-chart.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, ViewChild } from '@angular/core';
-import { DxScrollViewComponent } from 'devextreme-angular/ui/scroll-view';
+import { DxScrollViewComponent } from 'devextreme-angular';
 
 @Component({
     selector: 'app-detailed-chart',
